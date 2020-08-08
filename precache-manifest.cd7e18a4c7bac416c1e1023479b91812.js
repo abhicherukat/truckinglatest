2e02b36540bb24e3fa382b1a3c14dfc46076656d
abhi <abhideepak22@gmail.com>
2020-08-08 13:05:28 +0530

    Updates

---
+++ precache-manifest.cd7e18a4c7bac416c1e1023479b91812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358e675de0c42a293ad95b8440a398ec",
+    "url": "/truckinglatest/index.html"
+  },
+  {
+    "revision": "c020fe283a8d730d59e7",
+    "url": "/truckinglatest/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "099908b15e86adcbfb61",
+    "url": "/truckinglatest/static/css/main.96652d5b.chunk.css"
+  },
+  {
+    "revision": "c020fe283a8d730d59e7",
+    "url": "/truckinglatest/static/js/2.94e3b9d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/truckinglatest/static/js/2.94e3b9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099908b15e86adcbfb61",
+    "url": "/truckinglatest/static/js/main.ae1ce6ed.chunk.js"
+  },
+  {
+    "revision": "fec6c7d596951e1b6c48",
+    "url": "/truckinglatest/static/js/runtime-main.b6eda9bf.js"
+  }
+]);
